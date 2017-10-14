11a9f26b046638ab6df74f55b0f59de7237b6794
augustonildo <augustocarvalhopp@gmail.com>
2017-10-14 13:48:08 -0300

    Correção de acordo com novo BD

--- AMP/ObrasDoAcervo/src/obrasdoacervo/Periodicos.java
+++ AMP/ObrasDoAcervo/src/obrasdoacervo/Periodicos.java
@@ -10,13 +10,15 @@ package obrasdoacervo;
  * @author Aluno
  */
 public class Periodicos extends Obras {
-    int mes;
+    String periodicidade;
+    String mes;
     int volume;
     String subtipo;
     int ISSN;
 
-    public Periodicos(int mes, int volume, String subtipo, int ISSN, int id, int idObra, int idCampi, String nome, String tipo, String local, int ano, String editora, int paginas) {
-        super(id, idObra, idCampi, nome, tipo, local, ano, editora, paginas);
+    public Periodicos(String periodicidade, String mes, int volume, String subtipo, int ISSN, int idObra, int idCampi, String nome, String tipo, String local, String ano, String editora, String paginas) {
+        super(idObra, idCampi, nome, tipo, local, ano, editora, paginas);
+        this.periodicidade = periodicidade;
         this.mes = mes;
         this.volume = volume;
         this.subtipo = subtipo;
@@ -25,11 +27,12 @@ public class Periodicos extends Obras {
 
 
 
-    public int getMes() {
+
+    public String getMes() {
         return mes;
     }
 
-    public void setMes(int mes) {
+    public void setMes(String mes) {
         this.mes = mes;
     }
 

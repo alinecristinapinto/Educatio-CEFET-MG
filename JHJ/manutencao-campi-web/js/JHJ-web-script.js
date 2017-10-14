824101e31333ad29b34c4e8392fec5970e7d5572
joicesena <joice.sena.502@gmail.com>
2017-10-13 23:16:53 -0300

    Atualizando arquivo JavaScript externo

--- JHJ/manutencao-campi-web/js/JHJ-web-script.js
+++ JHJ/manutencao-campi-web/js/JHJ-web-script.js
@@ -1,3 +1,19 @@
 function voltarParaPaginaInsersaoCampus(){
-    location.href="JHJ-web-adicionar-campus.html"
+    location.href = "JHJ-web-adicionar-campus.html"
+}
+
+$(document).ready( function(){
+	$("#alertaSelecioneCampus").hide();
+    $("#alertaConfirmaExclusao").hide();
+    $("#botaoExcluirCampus").click(function() {
+    	if ($('#selectParaExcluirCampus').val() == ''){
+    		$("#alertaSelecioneCampus").show();
+    	} else {
+        	$("#alertaConfirmaExclusao").show();
+        }
+    });
+});
+
+function voltarParaPaginaExclusaoCampus(){
+	location.href = "JHJ-web-remover-campus.php"
 }

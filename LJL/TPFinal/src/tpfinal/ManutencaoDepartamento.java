3893e87aba98522e46594b96cf3ca84f12f4fa5c
Leonardo Gomide <leocgomide@gmail.com>
2017-10-16 11:36:19 -0200

    Adicionado Interação com BD (JAVA)

--- LJL/TPFinal/src/tpfinal/ManutencaoDepartamento.java
+++ LJL/TPFinal/src/tpfinal/ManutencaoDepartamento.java
@@ -1,12 +1,20 @@
 package tpfinal;
 
+import java.sql.*;
 import javax.swing.JOptionPane;
 
 
 public class ManutencaoDepartamento {
     
-    public Departamento CriarDepartamento(){
-        
+    public void CriarDepartamento() throws SQLException{
+        String sql = null;
+        Conexão conn = new Conexão();
+        Connection connection = conn.getConnection();
+        if(connection!=null){
+            
+        }else{
+            System.out.println("deu ruim :(");
+        }
         
         int IdCampus = 0;
         String nome = null;
@@ -28,7 +36,7 @@ public class ManutencaoDepartamento {
             fl = false;
             try{
                 nome = JOptionPane.showInputDialog("Nome do novo Departamento");
-                if (nome == null){
+                if (nome == null || nome == ""){
                     throw new ExcecaoNomeNulo("Dê um nome ao departamento!");
                 }
             }catch (ExcecaoNomeNulo e){
@@ -37,62 +45,85 @@ public class ManutencaoDepartamento {
             }
         }while(fl==true);
         
+        Departamento temp = new Departamento (IdCampus, nome);
         
         Object[] options = { "Confirmar", "Cancelar" };
         if (JOptionPane.showOptionDialog(null, "Confirme a criação:\nLocalizado no campus: "+IdCampus+"\nNome: "+nome, "Confirmação", JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, options, options[0]) == 0){
-            Departamento depto = new Departamento (IdCampus, nome);
-            return depto;
+                sql = "INSERT INTO `deptos`"
+                      + "(`id`, `idCampi`, `nome`, `ativo`)"
+                      + "VALUES (NULL, ?, ?, ?)";
         }
-        return null;
+        
+        try {
+		        // prepared statement para inser��o
+		        PreparedStatement stmt = connection.prepareStatement(sql);
+
+		        // seta os valores
+
+		        stmt.setInt(1, temp.getIdCampi());
+		        stmt.setString(2, temp.getNome());
+		        stmt.setString(3, "s");
+
+		        // executa
+		        stmt.execute();
+		        stmt.close();
+		    } catch (SQLException e) {
+		        throw new RuntimeException(e);
+		    }
         
     }
     
-    public Departamento CriarDepartamento(int IdCampus, String nome){
-        
-        Object[] options = { "Confirmar", "Cancelar" };
-        if (JOptionPane.showOptionDialog(null, "Confirme a criação:\nLocalizado no campus: "+IdCampus+"\nNome: "+nome, "Confirmação", JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, options, options[0]) == 0){
-            Departamento depto = new Departamento (1, "null");
-            return depto;
+    
+    public void AlterarDepartamento(){
+        String sql = null;
+        Conexão conn = new Conexão();
+        Connection connection = conn.getConnection();
+        if(connection!=null){
+            
+        }else{
+            System.out.println("deu ruim :(");
         }
-        return null;
-        
-    }
-    
-    
-    public void AlterarDepartamento(Departamento[] depto){
+        int opc = 0;
         int IDAlterar = 0;
+        String NmAlterar = null;
         boolean fl = false;
         boolean flagID = false;
         boolean flagExc;
         boolean flag = false;
+        String auxIdNome = null;
+        String auxData = null;
         int i;
+        int dataInt = 0;
+        String dataString = null;
         
-        try{
-            IDAlterar = Integer.parseInt(JOptionPane.showInputDialog("Digite o ID do departamento a ser alterado"));
-            for (i=0; i<depto.length; i++){
-                if(depto[i].getId()==IDAlterar && flagID == false){
-                    IDAlterar = i;
-                    flagID = true;
-                }
-            }
-        }
-        catch(NumberFormatException e){
-            fl = true;
-        }
+        
+        Object[] options = { "ID", "NOME" };
+        opc = JOptionPane.showOptionDialog(null, "ID ou Nome?", "ID/Nome",  JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, options, options[0]);
+        if (opc == 0)
+            IDAlterar = Integer.parseInt(JOptionPane.showInputDialog("Qual departamento será alterado?"));
+        else
+            NmAlterar = JOptionPane.showInputDialog("Qual departamento será alterado?");
+        
+        
+        if(opc == 0){
+            auxIdNome = "`id` = '"+IDAlterar+"'";
+	}else{
+            auxIdNome = "`nome` = '"+NmAlterar+"'";
+	}
 
         //if (fl = false && flagID == true){
-            Object[] options = { "Confirmar", "Cancelar" };
-            if (JOptionPane.showOptionDialog(null, "Deseja realmente alterar o departamento?\nID do departamento: "+depto[IDAlterar].getId()+"\nLocalizado no campus: "+depto[IDAlterar].getIdCampi()+"\nNome: "+depto[IDAlterar].getNome(), "Confirmação", JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, options, options[0]) == 0){
+            Object[] option = { "Confirmar", "Cancelar" };
+            if (JOptionPane.showOptionDialog(null, "Deseja realmente alterar o departamento?", "Confirmação", JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, option, option[0]) == 0){
             
-                Object[] option = { "Nome", "Campus", "Cancelar" };
-                int selecionado = JOptionPane.showOptionDialog(null, "O que gostaria de alterar?", "Alterar", JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, option, option[0]);
+                Object[] optionn = { "Nome", "Campus", "Cancelar" };
+                int selecionado = JOptionPane.showOptionDialog(null, "O que gostaria de alterar?", "Alterar", JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, optionn, optionn[0]);
             
                 if (selecionado == 0){
                     do{
                         flagExc = false;
                         try{
-                            depto[IDAlterar].setNome(JOptionPane.showInputDialog("Digite o novo nome"));
-                            if (depto[IDAlterar].getNome() == null){
+                            dataString = (JOptionPane.showInputDialog("Digite o novo nome"));
+                            if (dataString == null){
                                 throw new ExcecaoNomeNulo("Digite um nome!");
                             }
                         }
@@ -101,43 +132,123 @@ public class ManutencaoDepartamento {
                         flagExc=true;
                         }
                     }while(flagExc==true);
-            
-                    if (selecionado == 1){
-                        depto[IDAlterar].setIdCampi(Integer.parseInt(JOptionPane.showInputDialog("Digite o novo campus")));
-                    }
-                
+                    
+                    auxData = " SET `nome` = '"+dataString+"'";
+                    
                 }
+                else{
+                    if (selecionado == 1){
+                        try{
+                            dataInt = Integer.parseInt(JOptionPane.showInputDialog("Digite o novo campus"));
+                        }catch (NumberFormatException e){
+                            System.err.println(e);
+                        }
+                        auxData = " SET `idCampi` = '"+dataInt+"'";
+                    }
+                }
+                sql = "UPDATE `deptos`"
+                + auxData
+                + " WHERE `deptos`."+auxIdNome;
+                try {
+		    // prepared statement para inser��o
+		    PreparedStatement stmt = connection.prepareStatement(sql);
+                    stmt.execute();
+		    stmt.close();
+                } catch (SQLException e) {
+		    throw new RuntimeException(e);
+		}
             }
         //}
     }
     
-    public void InativarDepartamento(Departamento[] depto){
+    public void InativarDepartamento(){
+        
+        String sql = null;
+        Conexão conn = new Conexão();
+        Connection connection = conn.getConnection();
+        if(connection!=null){
+            
+        }else{
+            System.out.println("deu ruim :(");
+        }
         
         int i;
         int IDDel = 0;
-        boolean fl = false;
+        String NmDel = null;
+        String aux = null;
+        int opc = 0;
         boolean flagID = false;
         
-        try{
-            IDDel = Integer.parseInt(JOptionPane.showInputDialog("Digite o ID do departamento a ser Desativado"));
-            for (i=0; i<depto.length; i++){
-                if(depto[i].getId()==IDDel && flagID == false){
-                    IDDel = i;
-                    flagID = true;
-                }
-            }
-        }
-        catch(NumberFormatException e){
-            fl = true;
-        }
+        Object[] options = { "ID", "NOME" };
+        opc = JOptionPane.showOptionDialog(null, "ID ou Nome?", "ID/Nome",  JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, options, options[0]);
+        if (opc == 0)
+            IDDel = Integer.parseInt(JOptionPane.showInputDialog("Qual departamento será Desativado?"));
+        else
+            NmDel = JOptionPane.showInputDialog("Qual departamento será Desativado?");
         
+        if(opc == 0){
+            aux = "`id` = '"+IDDel+"'";
+	}else{
+            aux = "`nome` = '"+NmDel+"'";
+	}
         //if (fl = false && flagID == true){
-            Object[] options = { "Confirmar", "Cancelar" };
-            if (JOptionPane.showOptionDialog(null, "Deseja realmente alterar o departamento?\nID do departamento: "+depto[IDDel].getId()+"\nLocalizado no campus: "+depto[IDDel].getIdCampi()+"\nNome: "+depto[IDDel].getNome(), "Confirmação", JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, options, options[0]) == 0){
-                depto[IDDel].setAtivo(false);
+            Object[] option = { "Confirmar", "Cancelar" };
+            if (JOptionPane.showOptionDialog(null, "Deseja realmente desativar o departamento?", "Confirmação", JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, option, option[0]) == 0){
+                sql = "UPDATE `deptos`"
+                + " SET `ativo` = 'n'"
+                + " WHERE `deptos`."+aux;
+                try {
+		    // prepared statement para inser��o
+		    PreparedStatement stmt = connection.prepareStatement(sql);
+                    stmt.execute();
+		    stmt.close();
+                } catch (SQLException e) {
+		    throw new RuntimeException(e);
+		}
                 //desativar outros grupos
             }
         //}    
     }
+    /*
+    public String ProcurarDepartamento (){
+        
+        String sql = null;
+        Conexão conn = new Conexão();
+        Connection connection = conn.getConnection();
+        if(connection!=null){
+            
+        }else{
+            System.out.println("deu ruim :(");
+        }
+        
+        int ID;
+        String Nm;
+        int opc;
+        
+       
+        Object[] options = { "ID", "NOME" };
+        opc = JOptionPane.showOptionDialog(null, "ID ou Nome?", "ID/Nome",  JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, options, options[0]);
+        if (opc == 0)
+            ID = Integer.parseInt(JOptionPane.showInputDialog("Qual departamento?"));
+        else
+            Nm = JOptionPane.showInputDialog("Qual departamento?");
+        
+        
+        
+                for (i=0; i<depto.length; i++){
+                    if(depto[i]!=null){
+                    if(depto[i].getId()==IDDept && flag == false){
+                        IDDept = i;
+                        flag = true;
+                    }
+                }
+                }
+                }
+                catch(NumberFormatException e){
+                fl = true;
+                }
+            }while (fl == true);
+            JOptionPane.showMessageDialog(null, depto[IDDept].paraString());
+    }*/
     
 }
--- LJL/TPFinal/src/tpfinal/TPFinal.java
+++ LJL/TPFinal/src/tpfinal/TPFinal.java
@@ -1,74 +1,54 @@
 package tpfinal;
 
+import java.sql.*;
 import javax.swing.JOptionPane;
 
 
 public class TPFinal {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
         
         int i;
         int j = 0;
         Departamento[] depto = new Departamento[10];
+        for(i=0; i<depto.length; i++)
+            depto[i]=null;
         int selecionado;
         boolean flag = false;
         boolean fl;
+
         
         TransferirAluno aluno  = new TransferirAluno();
         ManutencaoDepartamento manDep = new ManutencaoDepartamento();
-        depto[j] = manDep.CriarDepartamento();
-        j++;
+        
+        //depto[j] = manDep.CriarDepartamento();
+        //j++;
+        
         do{
-        Object[] option = { "Criar", "Alterar", "Desativar", "Procurar Departamento", "Mostrar Dados", "Transferir Aluno", "Cancelar" };
-        selecionado = JOptionPane.showOptionDialog(null, "O que gostaria de alterar?", "Alterar", JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, option, option[0]);
+        Object[] option = { "Criar", "Alterar", "Desativar", "Transferir Aluno", "Cancelar" };
+        selecionado = JOptionPane.showOptionDialog(null, "O que gostaria de executar?", "Chamando Função", JOptionPane.DEFAULT_OPTION, JOptionPane.QUESTION_MESSAGE, null, option, option[0]);
         
         if (selecionado == 0){
-            depto[j]=manDep.CriarDepartamento();
-            j++;
+            manDep.CriarDepartamento();
+            
         }
         
         if (selecionado == 1){
-            manDep.AlterarDepartamento(depto);
+            manDep.AlterarDepartamento();
         }
         
         if (selecionado == 2){
-            manDep.InativarDepartamento(depto);
+            manDep.InativarDepartamento();
         }
         
-        int IDDept = 0;
+        /*if (selecionado == 3){
+            manDep.
+        }*/
+        
         if (selecionado == 3){
-            do{
-                fl = false;
-                try{
-                IDDept = Integer.parseInt(JOptionPane.showInputDialog("Digite o ID do departamento"));
-                for (i=0; i<depto.length; i++){
-                    if(depto[i].getId()==IDDept && flag == false){
-                        IDDept = i;
-                        flag = true;
-                    }
-                }
-                }
-                catch(NumberFormatException e){
-                fl = true;
-                }
-            }while (fl == true);
-            JOptionPane.showMessageDialog(null, depto[IDDept].paraString());
-        }
-        
-        if(selecionado == 4){
-            String tudo = "";
-            for (i=0; i<depto.length; i++){
-                if (depto[i] != null){
-                tudo += "\n\n\n"+depto[i].paraString();
-                }
-            }
-            JOptionPane.showMessageDialog(null, tudo);
-        }
-        
-        if (selecionado == 5){
             aluno.TransferirAluno();
         }
-        }while (selecionado != 6);
+        }while (selecionado != 4);
     }
     
 }

a94c79d44cf641d4654fb70c24164fd804874ccf
Dlinha <ninanerd15@gmail.com>
2017-10-23 22:14:11 -0200

    Atualizando meu repositório

---
+++ Bootstrap/bootstrap-4.0.0-alpha.6-dist/grupo-web-funcao-do-arquivo.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Funcao do Arquivo</title>
+        <meta charset="utf-8">
+        <link href="css/bootstrap.css" rel="stylesheet">
+        <link href="css/grupo-web-estilos.css" rel="stylesheet">
+        <script src="js/jquery.min.js"></script>
+        <script src="js/bootstrap.min.js"></script> 
+    </head>
+    <body>
+        
+        <!--
+            Grupo:​ ​ <nome_grupo>
+            Data​ ​ de​ ​ modificação:​ ​ <dia>/<mes>/<ano>
+            Autor:​ ​ <nome><sobrenome>
+​ ​ ​ ​ ​ ​ ​ ​ ​       Objetivo​ ​ da​ ​ modificação:​ ​ {objetivos}
+        -->
+    	
+        <input type="button" class="btn btn-outline-primary" value="Sem mouse"/>
+
+	<button class="btn btn-outline-primary">Com mouse</button>
+    </body>
+</html>

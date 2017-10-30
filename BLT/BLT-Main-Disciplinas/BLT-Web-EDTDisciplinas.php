5375d647df82d37163ef50661aaba223886dd4ae
Bryann360 <bryannbueno@hotmail.com>
2017-10-30 11:26:07 -0200

    Finalização back-end WEB

--- BLT/BLT-Main-Disciplinas/BLT-Web-EDTDisciplinas.php
+++ BLT/BLT-Main-Disciplinas/BLT-Web-EDTDisciplinas.php
@@ -5,14 +5,14 @@
   <meta charset="utf-8">
   <link href="css/bootstrap.css" rel="stylesheet">
   <link href="gerencia-web-estilos-rodape.css" rel="stylesheet">
-  <link href="BLT-Web-Emprestimos.css" rel="stylesheet">
+  <link href="BLT-Web-Disciplinas.css" rel="stylesheet">
   <script src="js/jquery.min.js"></script>
   <script src="js/bootstrap.min.js"></script> 
  </head>
 <body>
 <div class="corpo">
   <div class="titulo">
-  <h1><b>Gerenciamento de Empréstimos</b></h1>
+  <h1><b>Gerenciamento de Disciplinas</b></h1>
   </div>
 </div>
 </body>
@@ -33,7 +33,7 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT idAluno, idAcervo, dataEmprestimo, dataPrevisaoDevolucao, dataDevolucao, multa FROM emprestimos WHERE ativo = 'S'";
+$sql = "SELECT idTurma, nome, cargaHorariaMin FROM disciplinas WHERE ativo = 'S'";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
@@ -41,7 +41,7 @@ if ($result->num_rows > 0) {
     echo "<div class=\"TabelaExclusao\">";
     echo "<ul class=\"list-group\">";
     while($row = $result->fetch_assoc()) {
-    	echo "<li class=\"list-group-item align-items-center\">ID Aluno: ".$row["idAluno"]." | ID Acervo: ".$row["idAcervo"]." | Data empréstimo: ".$row["dataEmprestimo"]."</li>
+    	echo "<li class=\"list-group-item align-items-center\">ID Turma: ".$row["idTurma"]." | Nome da disciplina: ".$row["nome"]." | Carga horária mínima: ".$row["cargaHorariaMin"]."</li>
         </ul>
         </div>
         </div>";
@@ -49,30 +49,39 @@ if ($result->num_rows > 0) {
   echo "<body>
           <div class=\"margem-DLTEmp\">
           <div class=\"container-fluid\">
-          <form action=\"BLT-Web-DLTEmprestimos2.php\" method=\"post\">
+          <form action=\"BLT-Web-EDTDisciplinas2.php\" method=\"post\">
               <div class=\"row\" style=\"margin: 70px;\">
                 <div class=\"col-md-6 mb-3\">
-                    <label for=\"validationServer02\">ID do acervo que deseja devolver</label>
-                    <input type=\"text\" class=\"form-control is-valid\" id=\"validationServer02\" name=\"IDAcervo\" placeholder=\"ID do acervo\" value=\"\" required>
+                    <label for=\"validationServer02\">Nome da disciplina que deseja editar</label>
+                    <input type=\"text\" class=\"form-control is-valid\" id=\"validationServer02\" name=\"nome\" placeholder=\"Nome da disciplina\" value=\"\" required>
+
                   </div>
 
                   <div class=\"col-md-6 mb-3\">
+                  <label for=\"validationServer02\">ID da turma que vai editar a disciplina</label>
+                    <input type=\"text\" class=\"form-control is-valid\" id=\"validationServer02\" name=\"idTurma\" placeholder=\"ID da turma\" value=\"\" required>
+                    </div>
+                    <div class=\"col-md-12 mb-3\">
                     <label for=\"validationServer02\"></label>
-                    <button type=\"button\" data-toggle=\"modal\" data-target=\"#exampleModal\" class=\"btn btn-outline-info btn-block btn-lg\">Remover</button>
+                    <button type=\"button\" data-toggle=\"modal\" data-target=\"#exampleModal\" class=\"btn btn-outline-info btn-block btn-lg\">Editar</button>
 
-                      <div class=\"modal fade\" id=\"exampleModal\" tabindex=\"-1\" role=\"dialog\" aria-labelledby=\"exampleModalLabel\" aria-hidden=\"true\">
+                    <div class=\"modal fade\" id=\"exampleModal\" tabindex=\"-1\" role=\"dialog\" aria-labelledby=\"exampleModalLabel\" aria-hidden=\"true\">
                         <div class=\"modal-dialog\" role=\"document\">
                           <div class=\"modal-content\">
                             <div class=\"modal-header\">
-                              <h5 class=\"modal-title\" id=\"exampleModalLabel\">Modal title</h5>
+                              <h5 class=\"modal-title\" id=\"exampleModalLabel\">Edição de disciplina</h5>
                               <button type=\"button\" class=\"close\" data-dismiss=\"modal\" aria-label=\"Close\">
                                 <span aria-hidden=\"true\">&times;</span>
                               </button>
                             </div>
                             <div class=\"modal-body\">
                                 <div class=\"col-md-6 mb-3\">
-                                  <label for=\"validationServer02\">Data de devolução</label>
-                                  <input type=\"date\" name=\"datadevolucao\" class=\"form-control is-valid\" id=\"validationServer02\" placeholder=\"ID\" value=\"\" required>
+                                  <label for=\"validationServer02\">Nome da disciplina</label>
+                                  <input type=\"text\" name=\"nomeedt\" class=\"form-control is-valid\" id=\"validationServer02\" placeholder=\"Nome\" value=\"\" required>
+                                  <label for=\"validationServer02\">Id da turma</label>
+                                  <input type=\"number\" name=\"idTurmaedt\" class=\"form-control is-valid\" id=\"validationServer02\" placeholder=\"ID\" value=\"\" required>
+                                  <label for=\"validationServer02\">Carga horária mínima</label>
+                                  <input type=\"number\" name=\"carga\" class=\"form-control is-valid\" id=\"validationServer02\" placeholder=\"Horas\" value=\"\" required>
                                 </div>
                             </div>
                             <div class=\"modal-footer\">
@@ -83,10 +92,13 @@ if ($result->num_rows > 0) {
                         </div>
                       </div>
                     </div>
+                     </div>
+                    </div>
+                   </div>
                   </div>
-                </form>  
-              </div>
-            </div>
+           </form>  
+          </div>
+          </div>
           </body>";
 } else {
     echo "<div class=\"corpo\">";
@@ -97,7 +109,7 @@ if ($result->num_rows > 0) {
     <div class=\"container-fluid\">
       <div class=\"row\">
         <div class=\"col-md-12 mb-3\">
-          <button type=\"button\" class=\"btn btn-outline-info btn-block \" onclick=\"window.location.href='BLT-Web-Emprestimos.html'\">Pronto</button>
+          <button type=\"button\" class=\"btn btn-outline-info btn-block \" onclick=\"window.location.href='BLT-Web-Disciplinas.html'\">Pronto</button>
         </div>
       </div>
       </div>";
--- BLT/BLT-Main-Disciplinas/BLT-Web-EDTDisciplinas2.php
+++ BLT/BLT-Main-Disciplinas/BLT-Web-EDTDisciplinas2.php
@@ -5,13 +5,13 @@
   <meta charset="utf-8">
   <link href="css/bootstrap.css" rel="stylesheet">
   <link href="gerencia-web-estilos-rodape.css" rel="stylesheet">
-  <link href="BLT-Web-Emprestimos.css" rel="stylesheet">
+  <link href="BLT-Web-Disciplinas.css" rel="stylesheet">
   <script src="js/jquery.min.js"></script>
   <script src="js/bootstrap.min.js"></script> 
  </head>
 </html>
-
-
+<body>
+</body>
 
 
 
@@ -29,42 +29,26 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$dataDevolucao = $_POST['datadevolucao'];
-$IDacervo = $_POST['IDAcervo'];
+
+$nomeedt = $_POST['nomeedt'];
+$IDTurmaedt = $_POST['idTurmaedt'];
+$cargaedt = $_POST['carga'];
+$nome = $_POST['nome'];
+$IDTurma = $_POST['idTurma'];
 
 
 
-
-
-$sql = "SELECT dataPrevisaoDevolucao, multa FROM emprestimos WHERE idAcervo = '$IDacervo'";
-$result = $conn->query($sql);
-$multa= 0;
-
-if ($result->num_rows > 0) {
-    while($row = $result->fetch_assoc()) {
-
-      $dataPrazo= date_create($row["dataPrevisaoDevolucao"]);
-      $dataEntrega= date_create($dataDevolucao);
-      $diff=date_diff($dataEntrega,$dataPrazo);
-      if($diff->format("%R%a")>=0){
-        echo "Entregue no prazo!";
-      }else{
-        $multa = $diff->format("%a")*$row["multa"];
-      }
-
-
-        $sql = "UPDATE emprestimos SET ativo='N' WHERE idAcervo='$IDacervo'";
+        $sql = "UPDATE disciplinas SET nome='$nomeedt', idTurma='$IDTurmaedt', cargaHorariaMin='$cargaedt' WHERE nome='$nome' AND idTurma='$IDTurma'";
         if ($conn->query($sql) === TRUE) {
           echo "<div class=\"corpo\">";
           echo "<div class=\"titulo\">";
           echo "<h1>";
-          echo "<b>Livro Removido com sucesso!</b>";
-          echo "<br><br><h3>Multa a ser paga: ".$multa."R$</h3>";
+          echo "<b>Disciplina editada com sucesso!</b>";
           echo "</h1>";
           echo "<div class=\"container-fluid\">
                 <div class=\"row\">
                 <div class=\"col-md-12 mb-3\">
-                <button style=\"margin-top: 70px;\"type=\"button\" class=\"btn btn-outline-info btn-block \" onclick=\"window.location.href='BLT-Web-Emprestimos.html'\">Pronto</button>
+                <button style=\"margin-top: 70px;\"type=\"button\" class=\"btn btn-outline-info btn-block \" onclick=\"window.location.href='BLT-Web-Disciplinas.html'\">Pronto</button>
                 </div>
                 </div>
                 </div>";
@@ -73,14 +57,6 @@ if ($result->num_rows > 0) {
         } else {
           echo "Error updating record: " . $conn->error;
         }
-
-    }
-
-
-
-} else {
-    echo "0 results";
-}
 
 $conn->close();
 ?>

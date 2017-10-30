19dc63393648d5eaa0ef339ed9a640210decd7d2
joicesena <joice.sena.502@gmail.com>
2017-10-30 02:48:21 -0200

    Adicionando pastas css, fonts e js para o menu de relatórios

---
+++ JHJ/menu-relatorios-web/js/JHJ-web-script-relatorio9-tabela.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+    $('.filterable .btn-filter').click(function(){
+        var $panel = $(this).parents('.filterable'),
+        $filters = $panel.find('.filters input'),
+        $tbody = $panel.find('.table tbody');
+        if ($filters.prop('disabled') == true) {
+            $filters.prop('disabled', false);
+            $filters.first().focus();
+        } else {
+            $filters.val('').prop('disabled', true);
+            $tbody.find('.no-result').remove();
+            $tbody.find('tr').show();
+        }
+    });
+
+    $('.filterable .filters input').keyup(function(e){
+        var code = e.keyCode || e.which;
+        if (code == '9') return;
+        var $input = $(this),
+        inputContent = $input.val().toLowerCase(),
+        $panel = $input.parents('.filterable'),
+        column = $panel.find('.filters th').index($input.parents('th')),
+        $table = $panel.find('.table'),
+        $rows = $table.find('tbody tr');
+        var $filteredRows = $rows.filter(function(){
+            var value = $(this).find('td').eq(column).text().toLowerCase();
+            return value.indexOf(inputContent) === -1;
+        });
+        $table.find('tbody .no-result').remove();
+        $rows.show();
+        $filteredRows.hide();
+        if ($filteredRows.length === $rows.length) {
+            $table.find('tbody').prepend($('<tr class="no-result text-center"><td colspan="'+ $table.find('.filters th').length +'">Nenhum resultado encontrado</td></tr>'));
+        }
+    });
+});

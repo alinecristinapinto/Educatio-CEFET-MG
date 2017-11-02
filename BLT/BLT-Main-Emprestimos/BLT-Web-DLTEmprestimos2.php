259d8fe0709fd9c5bb0ed7a5cfa60c62db861dff
Bryann_Bueno <bryannbueno@hotmail.com>
2017-11-02 08:50:19 -0200

    atualizando meu repositorio

--- BLT/BLT-Main-Emprestimos/BLT-Web-DLTEmprestimos2.php
+++ BLT/BLT-Main-Emprestimos/BLT-Web-DLTEmprestimos2.php
@@ -9,6 +9,8 @@
   <script src="js/jquery.min.js"></script>
   <script src="js/bootstrap.min.js"></script> 
  </head>
+ <body>
+ </body>
 </html>
 
 
@@ -36,7 +38,7 @@ $IDacervo = $_POST['IDAcervo'];
 
 
 
-$sql = "SELECT dataPrevisaoDevolucao, multa FROM emprestimos WHERE idAcervo = '$IDacervo'";
+$sql = "SELECT dataPrevisaoDevolucao, multa FROM emprestimos WHERE idAcervo = '$IDacervo' AND ativo='S'";
 $result = $conn->query($sql);
 $multa= 0;
 
@@ -47,7 +49,6 @@ if ($result->num_rows > 0) {
       $dataEntrega= date_create($dataDevolucao);
       $diff=date_diff($dataEntrega,$dataPrazo);
       if($diff->format("%R%a")>=0){
-        echo "Entregue no prazo!";
       }else{
         $multa = $diff->format("%a")*$row["multa"];
       }
@@ -55,6 +56,7 @@ if ($result->num_rows > 0) {
 
         $sql = "UPDATE emprestimos SET ativo='N' WHERE idAcervo='$IDacervo'";
         if ($conn->query($sql) === TRUE) {
+          if($multa > 0){
           echo "<div class=\"corpo\">";
           echo "<div class=\"titulo\">";
           echo "<h1>";
@@ -64,12 +66,34 @@ if ($result->num_rows > 0) {
           echo "<div class=\"container-fluid\">
                 <div class=\"row\">
                 <div class=\"col-md-12 mb-3\">
+<<<<<<< HEAD
                 <button style=\"margin-top: 70px;\"type=\"button\" class=\"btn btn-outline-info btn-block \" onclick=\"window.location.href='BLT-Web-Emprestimos.html'\">Pronto</button>
+=======
+                <div class=\"container-fluid\">
+                <button type=\"button\" style=\"margin-top: 70px;\" class=\"btn btn-outline-info btn-block \" onclick=\"window.location.href='BLT-Web-Emprestimos.html'\">Pronto</button>
+>>>>>>> atualizando meu repositorio
                 </div>
                 </div>
                 </div>";
           echo "</div>";
           echo "</div>";
+          }else{
+            echo "<div class=\"corpo\">";
+            echo "<div class=\"titulo\">";
+            echo "<h1>";
+            echo "<b>Livro Removido com sucesso!</b>";
+            echo "<br><br><h3>Acervo entregue no prazo correto!\"</h3>";
+            echo "</h1>";
+            echo "<div class=\"row\">
+                  <div class=\"col-md-12 mb-3\">
+                  <div class=\"container-fluid\">
+                  <button type=\"button\" style=\"margin-top: 70px;\"class=\"btn btn-outline-info btn-block \" onclick=\"window.location.href='BLT-Web-Emprestimos.html'\">Pronto</button>
+                  </div>
+                  </div>
+                  </div>";
+            echo "</div>";
+            echo "</div>";
+          }
         } else {
           echo "Error updating record: " . $conn->error;
         }
@@ -79,7 +103,20 @@ if ($result->num_rows > 0) {
 
 
 } else {
-    echo "0 results";
+    echo "<div class=\"corpo\">";
+            echo "<div class=\"titulo\">";
+            echo "<h1>";
+            echo "<b>Nem um registro ativo para esse acervo!</b>";
+            echo "</h1>";
+            echo "<div class=\"row\">
+                  <div class=\"col-md-12 mb-3\">
+                  <div class=\"container-fluid\">
+                  <button type=\"button\" style=\"margin-top: 70px;\"class=\"btn btn-outline-info btn-block \" onclick=\"window.location.href='BLT-Web-Emprestimos.html'\">Pronto</button>
+                  </div>
+                  </div>
+                  </div>";
+            echo "</div>";
+            echo "</div>";
 }
 
 $conn->close();

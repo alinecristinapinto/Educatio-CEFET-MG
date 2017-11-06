8028bf79d7a2f711678dd3c0a671e3024a0bc3b5
Bryann360 <bryannbueno@hotmail.com>
2017-11-06 10:51:31 -0200

    Adicionando funcionalidade de reserva

--- BancoDeDados/insercaoElementosBD.php
+++ BancoDeDados/insercaoElementosBD.php
@@ -4,7 +4,7 @@
 
 	$servername = "localhost";
 	$username = "root";
-	$password = "usbw";
+	$password = "";
 	$bd = "educatio";
 
 	$conexao = mysqli_connect($servername, $username, $password, $bd);

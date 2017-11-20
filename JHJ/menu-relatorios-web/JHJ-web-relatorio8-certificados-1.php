a3d561e5071c5b67a4e9c4edab2d070e955be553
joicesena <joice.sena.502@gmail.com>
2017-11-19 23:04:10 -0200

    Adicionando arquivos PHP referentes ao relatório 8

---
+++ JHJ/menu-relatorios-web/JHJ-web-relatorio8-certificados-1.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+	    <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
+	    <meta name="viewport" content="width=device-width" />
+
+	    <link rel="stylesheet" href="css/bootstrap.min.css">
+	    <link rel="stylesheet" href="css/bootstrap.css">
+	    <script src="js/popper.js"></script>
+	    <script src="js/jquery-3.2.1.js"></script>
+	    <script src="js/bootstrap.min.js"></script>
+	    <script src="js/JHJ-web-script.js" type="text/javascript"></script>
+	</head>
+	<body>		
+		<form action="JHJ-web-relatorio8-certificados-2.php" method="POST">
+			<input type="submit" name="opcaoMostrar" value="Mostrar Certificado" class="btn btn-info btn-round"/>
+		    <input type="submit" name="opcaoDownload" value="Download do Certificado em PDF" class="btn btn-info btn-round"/>
+		</form>
+	</body>
+</html>

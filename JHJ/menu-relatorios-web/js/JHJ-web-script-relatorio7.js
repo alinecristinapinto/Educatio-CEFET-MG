6f4827f4f687ad29e6372da565ce3d4025a86444
joicesena <joice.sena.502@gmail.com>
2017-11-26 09:52:13 -0200

    Adicionando arquivos referentes ao relatório 7 em WEB

---
+++ JHJ/menu-relatorios-web/js/JHJ-web-script-relatorio7.js
@@ -0,0 +1,6 @@
+function voltarParaPaginaSelecionarCampi(){
+    location.href = "JHJ-web-relatorio7-historico-1.php";
+}
+function irParaPaginaDownloadHistorico(){
+    location.href = "JHJ-web-relatorio7-historico-7.php";
+}
